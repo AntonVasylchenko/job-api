c65cfe6ca01f438242eea6edd834f5c0bde754a9
Vasylchenko Anton <87824792+Smd16Bi@users.noreply.github.com>
2024-03-18 08:16:59 +0200

    Added swagger

--- app.js
+++ app.js
@@ -8,6 +8,12 @@ const cors = require("cors");
 const xss = require("xss-clean");
 const rateLimiter = require("express-rate-limit");
 
+
+// Swagger
+
+const swaggerUI =require("swagger-ui-express");
+const YAML = require("yamljs")
+const swaggerDocument = YAML.load("./swagger.yaml")
 const express = require("express");
 const app = express();
 
@@ -35,6 +41,11 @@ app.use(helmet());
 app.use(cors());
 app.use(xss());
 // extra packages
+
+app.get("/", (req,res) => {
+  res.send("<h1>Api</h1><br><a href='/api-docs '>docs</a>")
+})
+app.use("/api-docs",swaggerUI.serve, swaggerUI.setup(swaggerDocument))
 
 // routes
 app.use("/api/v1/auth", authRouter);
